2c95fde3a867e8375e6897e46c8816fb82eb6f12
nievesag <nievalon@ucm.es>
2024-03-14 20:12:29 +0100

    maquinorra de estados

--- Alcachofita_2/Assets/Scripts/GameManager.cs
+++ Alcachofita_2/Assets/Scripts/GameManager.cs
@@ -1,39 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class GameManager : MonoBehaviour
 {
-    public enum GameStates { INTRO, GAME, END };
-
-    #region references
-    // aun no hay nada je
-    #endregion
+    public enum GameStates { MAINMENU, GAME, END };
 
     #region properties
+    // GAMEMANAGER
+    private static GameManager _gameManager;
+    public static GameManager Instance { get { return _gameManager; } }
+
+    // INPUT
+    /*
+    private InputManager _input;
+    public InputManager Input { get { return _input; } }
+    */
+
     // ESTADOS
-    private static GameManager _instance;
     private GameStates _currentGameState;
-    private GameStates _nextGameState;
-    public static GameManager Instance { get { return _instance; } }
     public GameStates CurrentState { get { return _currentGameState; } }
+
+    private GameStates _nextGameState;
     public GameStates NextState { get { return _nextGameState; } }
     #endregion
 
-    public void RequestSateChange(GameStates newState)
+    #region METODOS DE ESTADOS
+    // ---- requestStateChange ----
+    // establece cual es el estado siguiente al que ir
+    public void requestSateChange(GameStates newState)
+    {
+        // guarda el estado correspondiente en next
+        _nextGameState = newState;
+    }
+
+    // ---- onStateEnter ----
+    // decide que hacer en cada estado
+    public void onStateEnter(GameStates newState)
+    {
+        switch (newState)
+        {
+            // ---- MAIN MENU ----
+            case GameStates.MAINMENU:   
+
+                break;
+
+            // ---- GAME ----
+            case GameStates.GAME:       
+
+                break;
+
+            // ---- END ----
+            case GameStates.END:        
+
+                break;
+        }
+
+        // guarda el estado correspondiente en current
+        _currentGameState = newState;
+
+        Debug.Log("Nosss encontramoS en el eStado:" + _currentGameState);
+    }
+
+    // ---- updateState ----
+    // sobretodo para input y ui y cosas asi ?????
+    public void updateState(GameStates state)
     {
 
+    }
+    #endregion
+
+    private void Awake()
+    {
+        // si ya existe instancia del gamemanager se destruye
+        if (_gameManager != null) { Destroy(this); }
+
+        // en otro caso la asigna
+        else
+        {
+            _gameManager = this;
+
+            // si se guarda info en el gameManager y se ha de recargar
+            DontDestroyOnLoad(this);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // para cuando exista el input 
+        //_input = GetComponent<InputManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // si se debe cambiar de estado (next y current difieren)
+        if (_nextGameState != _currentGameState)
+        {
+            // se cambia
+            onStateEnter(_nextGameState);
+        }
+
+        // se actualiza el estado en el que se este
+        updateState(_currentGameState);
     }
 }
