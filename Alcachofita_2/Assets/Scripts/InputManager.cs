161c4ceb0af0dfc3c3b5e8662b035bbd91d7ccec
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-03-14 20:06:23 +0100

    Update InputManager.cs

--- Alcachofita_2/Assets/Scripts/InputManager.cs
+++ Alcachofita_2/Assets/Scripts/InputManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -13,6 +15,22 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("boton izquierdo");
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("boton derecho");
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("boton medio");
+        }
     }
 }
