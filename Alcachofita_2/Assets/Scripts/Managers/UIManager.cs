013fe989ec9919e27e82a2e87f15884fd8b6559a
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-03-15 09:57:28 +0100

    e. caputars goetia

--- Alcachofita_2/Assets/Scripts/Managers/UIManager.cs
+++ Alcachofita_2/Assets/Scripts/Managers/UIManager.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class UIManager : MonoBehaviour
 {
     // References
     [SerializeField] // sigue el orden de los estados del gamemanager
     public GameObject[] menus;
+
+    public void RequestStateChange(GameManager.GameStates state)
+    {
+        if (GameManager.Instance != null)
+            GameManager.Instance.requestSateChange(state);
+    }
+
+    public void GoToGame()
+    {
+        RequestStateChange(GameManager.GameStates.GAME); // referenciando al gamemanager (importante! si no no cambia de estado)
+        //...
+    }
+
+   public void ExitGame()
+    {
+        Application.Quit();
+    }
 
     // Start is called before the first frame update
     void Start()
