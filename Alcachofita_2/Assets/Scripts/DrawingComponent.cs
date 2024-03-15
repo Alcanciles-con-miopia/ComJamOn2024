fe1f91c349ba6b36ecc5ef10de8a30cd94344d33
Denisa Juarranz Berindea <denisaju@ucm.es>
2024-03-15 13:12:22 +0100

    Desesperación con los colores

--- Alcachofita_2/Assets/Scripts/DrawingComponent.cs
+++ Alcachofita_2/Assets/Scripts/DrawingComponent.cs
@@ -8,6 +8,11 @@ public class DrawingComponent : MonoBehaviour
     #region Parameters
 
     [SerializeField] float _minDistance = 0.1f;
+    [SerializeField] Material _material;
+    [SerializeField] float _startWidth = 0.5f;
+    [SerializeField] float _endWidth = 0.5f;
+    Color _startColor = new Color(55, 0, 0);
+    Color _endColor = new Color(217, 13, 13);
 
     #endregion
 
@@ -51,8 +56,27 @@ public class DrawingComponent : MonoBehaviour
         LineRenderer newLine = new GameObject().AddComponent<LineRenderer>();
         newLine.transform.SetParent(transform);
         line = newLine.GetComponent<LineRenderer>();
+
+        //Guarrada para los dos primeros puntos
         line.SetPosition(0, Camera.main.ScreenToWorldPoint(Input.mousePosition));
         line.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        //Color
+        Gradient colorGradient = new Gradient();
+        GradientColorKey[] colors = new GradientColorKey[2];
+        colors[0] = new GradientColorKey(_startColor, 0);
+        colors[1] = new GradientColorKey(_endColor, 1);
+        colorGradient.colorKeys = colors;
+        line.colorGradient = colorGradient;
+
+        //Materiales
+        List<Material> materials = new List<Material>();
+        materials.Add(_material);
+        line.SetMaterials(materials);
+   
+        //Tamaño
+        line.startWidth = _startWidth;
+        line.endWidth = _endWidth;
 
         //Creamos una lista para a�adir los puntos (luego lo pasamos a array)
         List<Vector3> puntos = new List<Vector3>();
