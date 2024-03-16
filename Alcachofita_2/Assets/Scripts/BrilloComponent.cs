9b5ecd596af9ba672ab54ec00b188746e9a2b2cc
nievesag <nievalon@ucm.es>
2024-03-16 19:28:57 +0100

    cyntriiiisttt

---
+++ Alcachofita_2/Assets/Scripts/BrilloComponent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrilloComponent : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        Debug.Log("pis");
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("caca");
+    }
+}
